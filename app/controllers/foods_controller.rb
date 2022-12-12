00309012b728f5a2c947e5f36f9d478d65926db2
Raga <ragawilliam570@gmail.com>
2022-12-12 18:59:51 +0300

    Add create new food controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -8,17 +8,17 @@ class FoodsController < ApplicationController
   end
 
   # def new
-  #   @new_food = Food.new
-  # end
+    @new_food = Food.new
+  end
 
-  # def create
-  #   food = Food.new(user: current_user, **post_params)
-  #   if food.save
-  #     redirect_to foods_path notice: 'Food added successfully'
-  #   else
-  #     render :new
-  #   end
-  # end
+  def create
+    food = Food.new(user: current_user, **post_params)
+    if food.save
+      redirect_to foods_path notice: 'Food added successfully'
+    else
+      render :new
+    end
+  end
 
   # def destroy
   #   food = Food.find(params[:id])
