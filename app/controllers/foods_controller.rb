9afbe0635026095759276e801181d027d62450c2
Raga <ragawilliam570@gmail.com>
2022-12-12 16:52:42 +0300

    Add Foods controller

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,19 @@
+class FoodsController < ApplicationController
+  def index
+    @foods = Food.all
+
+  end
+
+  def show
+    @food = Food.find(params[:id])
+    
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+end
