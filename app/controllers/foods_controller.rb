a7907231075c751be24d1c380f7c9b215610049b
Raga <ragawilliam570@gmail.com>
2022-12-12 18:58:49 +0300

    Add index and show controller methods for food

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,19 +1,37 @@
 class FoodsController < ApplicationController
   def index
     @foods = Food.all
-
   end
 
   def show
     @food = Food.find(params[:id])
-    
   end
 
-  def new
+  # def new
+  #   @new_food = Food.new
+  # end
 
-  end
+  # def create
+  #   food = Food.new(user: current_user, **post_params)
+  #   if food.save
+  #     redirect_to foods_path notice: 'Food added successfully'
+  #   else
+  #     render :new
+  #   end
+  # end
 
-  def create
+  # def destroy
+  #   food = Food.find(params[:id])
+  #   if food.destroy
+  #     flash[:notice] = 'Food deleted'
+  #   else
+  #     flash[:alert] = 'Couldn\'t delete food'
+  #   end
 
+  #   redirect_to foods_path
+  # end
+
+  def post_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
   end
 end
