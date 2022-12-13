0ce975d142ce7e1c33bc77113bc6687d748e18c4
Raga <ragawilliam570@gmail.com>
2022-12-13 13:57:23 +0300

    Add ability user checker

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,13 @@
 class Ability
   include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new
+
+    if user_signed_in?
+      can :read, :all
+      can :destroy, Food, user: user
+      can :destroy, Recipe, user: user
+    end
+  end
 end
