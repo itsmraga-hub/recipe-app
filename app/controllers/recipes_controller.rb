514bbd3b536a4cb75d1de52d8e866761ea8df76c
iamyunusali <aliyunus291@gmail.com>
2022-12-13 11:23:30 +0300

    fix linter errors

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,44 +1,42 @@
 class RecipesController < ApplicationController
-    def index
-      @recipes = current_user.recipes
+  def index
+    @recipes = current_user.recipes
+  end
+
+  def show
+    recipe_test = Recipe.find(params[:id])
+    return redirect_to recipes_path unless recipe_test.user == current_user
+
+    @recipe = Recipe.find(params[:id])
+    @recipe_foods = RecipeFood.where(recipe_id: @recipe.id).includes(:food, :recipe)
+  end
+
+  def destroy
+    recipe = Recipe.find(params[:id])
+
+    unless recipe.user == current_user
+      return flash[:alert] =
+               'You do not have access to delete the Recipe belongs to other Users.'
     end
 
-    def show
-      recipe_test = Recipe.find(params[:id])
-      unless recipe_test.user == current_user
-        return redirect_to recipes_path
-      end
-      @recipe = Recipe.find(params[:id])
-      @recipe_foods = RecipeFood.where(recipe_id: @recipe.id).includes(:food, :recipe)
+    if recipe.destroy
+      flash[:notice] = 'Recipe was successfully deleted.'
+    else
+      flash[:alert] = 'Recipe deleting Failed. Please try again.'
     end
+    redirect_back(fallback_location: root_path)
+  end
 
-    def destroy
-        recipe = Recipe.find(params[:id])
+  def create
+    recipe = current_user.recipes.new(recipe_params)
+    redirect_to recipes_path if recipe.save
+  end
 
-        unless recipe.user == current_user
-          return flash[:alert] =
-                   'You do not have access to delete the Recipe belongs to other Users.'
-        end
+  def new
+    @recipe = Recipe.new
+  end
 
-        if recipe.destroy
-          flash[:notice] = 'Recipe was successfully deleted.'
-        else
-          flash[:alert] = 'Recipe deleting Failed. Please try again.'
-        end
-        redirect_back(fallback_location: root_path)
-end
-
-    def create
-      recipe = current_user.recipes.new(recipe_params)
-      redirect_to recipes_path if recipe.save
-    end
-
-    def new
-      @recipe = Recipe.new
-    end
-
-
-    private
+  private
 
   def recipe_params
     params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
