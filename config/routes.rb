86bb35e7562a3d569813f459c558ff811cce856f
Raga <ragawilliam570@gmail.com>
2022-12-13 11:49:28 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
+
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: 'recipes#index'
+  # root to: 'recipes#index'
+  resources :users
   resources :recipes
+  resources :foods
 end
